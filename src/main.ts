bbb1d4aad1b3351ddb7b0876e808e953e0849fa5
TheProfanedOne <mjm11102001@gmail.com>
2023-03-12 17:58:07 -0600

    add icons

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
+import '@/assets/icons/sass/weather-icons.min.scss';
+import '@/assets/icons/sass/weather-icons-wind.min.scss';
+
 import App from './App.vue';
 import router from './router';
-
-import './assets/main.css';
 
 const app = createApp(App);
 
